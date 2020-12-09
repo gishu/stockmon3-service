a751a4dbfdf7bc1510b1aac18cb61b1a1acca2ec
gishu <gishu@hotmail.com>
2020-12-10 00:11:06 +0530

    new blank project

---
+++ src/stockmon3/core.clj
@@ -0,0 +1,7 @@
+(ns stockmon3.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/stockmon3/core_test.clj
@@ -0,0 +1,7 @@
+(ns stockmon3.core-test
+  (:require [clojure.test :refer :all]
+            [stockmon3.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
