75d7aa64f5587de3ed722730ec87121cd9f67926
Gishu Pillai <gishu@hotmail.com>
2021-05-10 12:08:36 +0530

    quotes to return type of inv (EQ, INDEX fund, etc)

--- src/stockmon3/domain/quotes.clj
+++ src/stockmon3/domain/quotes.clj
@@ -8,10 +8,12 @@
     (->> (csv/read-csv reader)
          csv-data->maps
          (reduce #(let [alias (:Alias %2)
+                        type (:Type %2)
                         alias (if (= "" alias) nil alias)
+                        type (if (= "" type) "EQ" type)
                         stock (or alias (:Code %2))
                         price (Double/parseDouble (:Price %2))]
-                    
-                    (assoc %1 stock price)) 
+
+                    (assoc %1 stock {:type type :price price})) 
                  {})
          doall)))
